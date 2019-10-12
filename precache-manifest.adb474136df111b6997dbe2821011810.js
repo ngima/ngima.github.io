6e83693ec3b3bcf2e90779f42a51c2fc21a10e50
Ngima Sherpa <ngimasherpa@Ngimas-MacBook-Pro.local>
2019-10-12 20:42:00 +0545

    update portfolio site

---
+++ precache-manifest.adb474136df111b6997dbe2821011810.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "33cf0aec9ea8b8392838",
+    "url": "/static/js/main.33cf0aec.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "33cf0aec9ea8b8392838",
+    "url": "/static/css/main.14ed588d.chunk.css"
+  },
+  {
+    "revision": "f339c0981cda8d6efa1fc9d2d48244f7",
+    "url": "/index.html"
+  }
+];
