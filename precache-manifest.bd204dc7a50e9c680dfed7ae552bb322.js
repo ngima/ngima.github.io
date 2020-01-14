c21340b44ad59907db5d37972008af2de4ae33e0
Ngima Sherpa <ngimasherpa@Ngimas-MacBook-Pro.local>
2020-01-14 09:21:49 +0545

    site update

---
+++ precache-manifest.bd204dc7a50e9c680dfed7ae552bb322.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71177826fbd7d9fcf34966a7e568131",
+    "url": "/black-dashboard-react/index.html"
+  },
+  {
+    "revision": "394280e0b5b826c82f1e",
+    "url": "/black-dashboard-react/static/css/2.9e63dcda.chunk.css"
+  },
+  {
+    "revision": "706b60aa9d3529944608",
+    "url": "/black-dashboard-react/static/css/main.26fbb27f.chunk.css"
+  },
+  {
+    "revision": "394280e0b5b826c82f1e",
+    "url": "/black-dashboard-react/static/js/2.c90ff011.chunk.js"
+  },
+  {
+    "revision": "2e90f24d6f9d6c7e6683a25b6535a902",
+    "url": "/black-dashboard-react/static/js/2.c90ff011.chunk.js.LICENSE"
+  },
+  {
+    "revision": "706b60aa9d3529944608",
+    "url": "/black-dashboard-react/static/js/main.bb0c90ad.chunk.js"
+  },
+  {
+    "revision": "67d2f5ba5143fe1affa48e55357bf584",
+    "url": "/black-dashboard-react/static/js/main.bb0c90ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0abc531fa884eb39374a",
+    "url": "/black-dashboard-react/static/js/runtime-main.9e9bff13.js"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/black-dashboard-react/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "9fcf69e5359987c9b1545aae1c2c5faa",
+    "url": "/black-dashboard-react/static/media/emilyz.9fcf69e5.jpg"
+  },
+  {
+    "revision": "205d552af49bdd1da7bf405ea6985816",
+    "url": "/black-dashboard-react/static/media/logo.205d552a.png"
+  },
+  {
+    "revision": "eee7d42bc0451eafa6bc447c0ef998b8",
+    "url": "/black-dashboard-react/static/media/ngima_sherpa.eee7d42b.jpg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/black-dashboard-react/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/black-dashboard-react/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/black-dashboard-react/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/black-dashboard-react/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/black-dashboard-react/static/media/react-logo.eb6be414.png"
+  }
+]);
